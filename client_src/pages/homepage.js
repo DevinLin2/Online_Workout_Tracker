4cd5c253b4b25b7a4502b9df2a70952e320844a0
Devin Lin <dxl702@case.edu>
2022-10-19 22:28:07 -0400

    Changed calendar and added input form for user data

--- client_src/pages/homepage.js
+++ client_src/pages/homepage.js
@@ -1,10 +1,33 @@
 import { useState } from "react";
-import Calendar from "react-calendar";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US")
+}
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales
+})
+
+const events = []
 
 export default function homepage() {
-    const [date, setDate] = useState(new Date());
-    var data = {
-        "username": "admin"
+    const [newEvent, setNewEvent] = useState({exercise: "", date: "", sets: "", reps: ""})
+    const [allEvents, setAllEvents] = useState(events)
+
+    function handleNewEvent() {
+        setAllEvents([...allEvents, newEvent])
     }
 
     function handleClick() {
@@ -16,18 +39,42 @@ export default function homepage() {
     }
 
     return (
-        <div className='app'>
-            <h1 className='text-center'>Workout Tracker</h1>
-            <div className='calendar-container'>
-                <Calendar onChange={setDate} value={date} />
+        <div>
+            <h1>Workout Calendar</h1>
+            <div>
+                <input 
+                    type="text" 
+                    placeholder="Add Exercise" 
+                    style={{width: "20%", marginRight: "10px"}}
+                    value={newEvent.exercise} onChange={(e) => setNewEvent({...newEvent, exercise: e.target.value})}
+                />
+                <input 
+                    type="text" 
+                    placeholder="Sets Done" 
+                    // style={{width: "20%", marginRight: "10px"}}
+                    value={newEvent.sets} onChange={(e) => setNewEvent({...newEvent, sets: e.target.value})}
+                />
+                <input 
+                    type="text" 
+                    placeholder="Reps Done" 
+                    // style={{width: "20%", marginRight: "10px"}}
+                    value={newEvent.reps} onChange={(e) => setNewEvent({...newEvent, reps: e.target.value})}
+                />
+                <DatePicker
+                    placeholderText="Date"
+                    style={{marginRight: "10px"}}
+                    selected={newEvent.date}
+                    onChange={(date) => setNewEvent({...newEvent, date})}
+                />
+                <button style={{marginTop: "10px"}} onClick={handleNewEvent}>Enter</button>
             </div>
-            <p className='text-center'>
-                <span className='bold'>Selected Date:</span>{' '}
-                {date.toDateString()}
-            </p>
-            <div onClick={handleClick}>
-                Send data to backend
-            </div>
+            <Calendar 
+                localizer={localizer} 
+                events={events}
+                startAccessor="date" 
+                endAccessor="date" 
+                style={{height: 500, margin: "50px"}}
+            />
         </div>
     );
 }
