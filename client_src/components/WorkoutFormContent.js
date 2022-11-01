df56e28b270d4d58b5e45a94553aaa0ebe622348
Devin Lin <dxl702@case.edu>
2022-11-01 13:22:18 -0400

    added component for the contents of the workout form popup

---
+++ client_src/components/WorkoutFormContent.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function WorkoutFormContent({show}) {
+    return (
+        <form>
+            <div>
+                <input type="text" id="workoutform1"></input>
+            </div>
+        </form>
+    )
+}
