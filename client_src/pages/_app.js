4dbaf342ac056421a67bb096bb5a844ac4484a7e
HoranS27 <115019646+HoranS27@users.noreply.github.com>
2022-11-06 14:35:46 -0500

    Update _app.js

--- client_src/pages/_app.js
+++ client_src/pages/_app.js
@@ -1,5 +1,6 @@
 import "../components/WorkoutForm.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../components/Login.css"
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
