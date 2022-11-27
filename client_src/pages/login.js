01ceb0056667505cfcad11169811e1023999dbff
HoranS27 <115019646+HoranS27@users.noreply.github.com>
2022-11-27 13:08:00 -0500

    Add files via upload

--- client_src/pages/login.js
+++ client_src/pages/login.js
@@ -1,57 +1,62 @@
-import { useState } from "react";
+import React from "react";
 
-const events = []
-
-function Login() {
-    const [newEvent, setNewEvent] = useState({username: "", password: ""})
-    const [allEvents, setAllEvents] = useState(events)
-
-    function sendData() {
-        //newEvent.username = "steve"
-        fetch('http://localhost:5000/insert', {
-            method: 'POST',
-            mode: 'cors',
-            body: JSON.stringify(newEvent)//allEvent?
-        })
+class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
     }
 
-    function handleNewEvent() {
-        setAllEvents([...allEvents, newEvent])
-        sendData();
+    handleLogin() {
+        if (this.state.username && this.state.password) {
+          alert("Welcome");
+          //need to be connected with the backend
+          //need to be connected with the router
+        } else {
+          alert("Please enter the username and password");
+        }
     }
-    
-    return (
+
+    render() {
+        return(
         <div className="login">
             <h1>Online Workout Tracker</h1>
-            <form className="form">
-                    <p>Username</p>
-                    <div className="formItem">
-                        <input
-                            label="Username" 
-                            type="text" 
-                            placeholder="Enter Username" 
-                            value={newEvent.username} onChange={(e) => setNewEvent({...newEvent, username: e.target.value})}
-                        />
-                    <span class="error"></span>
-                    </div>
+            <form className="form">s
+                <p>Username</p>
+                <div className="formItem">
+                    <input
+                        label="Username" 
+                        type="text" 
+                        placeholder="Enter Username"
+                        value={this.state.username}
+                        onChange={(e)=>{
+                            this.setState({username: e.target.value})
+                        }}
+                    />
+                </div>
 
-                    <p>Password</p>
-                    <div className="formItem">
-                        <input 
-                            type="text" 
-                            placeholder="Enter Password" 
-                            value={newEvent.exercise} onChange={(e) => setNewEvent({...newEvent, password: e.target.value})}
-                        />
-                    <span class="error"></span>
-                    </div>
+                <p>Password</p>
+                <div className="formItem">
+                    <input 
+                        type="text" 
+                        placeholder="Enter Password" 
+                        value={this.state.password}
+                        onChange={(e)=>{
+                            this.setState({password: e.target.value})
+                        }}
+                    />
+                </div>
 
-                    <div className="formItemBtn">
-                        <button className="loginBtn" onClick={()=>handleNewEvent}>Login</button>
-                    </div>
+                <div className="formItemBtn">
+                    <button className="loginBtn" onClick={() => {this.handleLogin();}}>Login</button>
+                </div>
 
             </form>
         </div>
-    );
+        );
+    }
 }
 
 export default Login;
--- client_src/pages/register.js
+++ client_src/pages/register.js
@@ -1,65 +1,78 @@
-import { useState } from "react";
+import React from "react";
 
-const events = []
-
-function Register() {
-    const [newEvent, setNewEvent] = useState({username: "", password: ""})
-    const [allEvents, setAllEvents] = useState(events)
-
-    function sendData() {
-        fetch('http://localhost:5000/register', {
-            method: 'POST',
-            mode: 'cors',
-            body: JSON.stringify(newEvent)
-        })
+class Register extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            Reenterpassword: ""
+        };
     }
 
-    function handleNewEvent() {
-        setAllEvents([...allEvents, newEvent])
-        sendData();
+    handleRegister() {
+        if (this.state.username=="") {
+          alert("Please enter the username");
+        } else if(this.state.password==""){
+          alert("Please enter the password");
+        } else if(this.state.Reenterpassword==""){
+          alert("Please Re-enter the password");
+        } else if(this.state.Reenterpassword!=this.state.password){
+          alert("The password is different from the Re-enter the password");
+        } 
+        //need to be connected with the router
     }
 
-    return (
-    <div className="login">
-        <h1>Online Workout Tracker</h1>
-        <h2>Register</h2>
-        <form className="form">
-                    <p>Username</p>
-                    <div className="formItem">
-                        <input 
-                            type="text" 
-                            placeholder="Enter Username(E-mail Address/Phone number)" 
-                            value={newEvent.username} onChange={(e) => setNewEvent({...newEvent, username: e.target.value})}
-                        />
-                    <span class="error"></span>
-                    </div>
+    render() {
+        return(
+        <div className="login">
+            <h1>Online Workout Tracker Register</h1>
+            <form className="form">
+                <p>Username</p>
+                <div className="formItem">
+                    <input
+                        label="Username" 
+                        type="text" 
+                        placeholder="Enter Username"
+                        value={this.state.username}
+                        onChange={(e)=>{
+                            this.setState({username: e.target.value})
+                        }}
+                    />
+                </div>
 
-                    <p>Password</p>
-                    <div className="formItem">
-                        <input 
-                            type="text" 
-                            placeholder="Enter Password" 
-                            value={newEvent.exercise} onChange={(e) => setNewEvent({...newEvent, password: e.target.value})}
-                        />
-                    <span class="error"></span>
-                    </div>
+                <p>Password</p>
+                <div className="formItem">
+                    <input 
+                        type="text" 
+                        placeholder="Enter Password" 
+                        value={this.state.password}
+                        onChange={(e)=>{
+                            this.setState({password: e.target.value})
+                        }}
+                    />
+                </div>
 
-                    <p>Re-enter Password</p>
-                    <div className="formItem">
-                        <input 
-                            type="text" 
-                            placeholder="Re-enter Password" 
-                            value={newEvent.exercise} onChange={(e) => setNewEvent({...newEvent, password: e.target.value})}
-                        />
-                    <span class="error"></span>
-                    </div>
+                <p>Re-Enter Password</p>
+                <div className="formItem">
+                    <input 
+                        type="text" 
+                        placeholder="Re-Enter Password" 
+                        value={this.state.Reenterpassword}
+                        onChange={(e)=>{
+                            this.setState({Reenterpassword: e.target.value})
+                        }}
+                    />
+                </div>
 
-                    <div className="formItemBtn">
-                        <button className="loginBtn" onClick={()=>handleNewEvent}>Register</button>
-                    </div>
+                <div className="formItemBtn">
+                    <button className="loginBtn" onClick={() => {this.handleRegister();}}>Register</button>
+                </div>
+
             </form>
-    </div>
-    );
+        </div>
+        );
+    }
 }
 
 export default Register;
