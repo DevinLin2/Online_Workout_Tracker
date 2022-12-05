e5a2193d82305dcfaaa9982e5c68cb756cfd9403
EddyTianyi <115376328+EddyTianyi@users.noreply.github.com>
2022-12-05 17:21:46 -0500

    Add files via upload

--- client_src/pages/nutritiongraph.js
+++ client_src/pages/nutritiongraph.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactEcharts from 'echarts-for-react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
@@ -6,37 +6,71 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faDumbbell,
 } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from 'next/router';
+import moment from 'moment';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import format from "date-fns/format";
 
-class workOutEcharts extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            piedata:[],
-            meal:[],
-            ID:''
-        };
+export async function getStaticProps() {
+    const res = await fetch('http://localhost:3000/api/mealHandler');
+    const props = await res.json();
+    return {
+        props: {
+            props,
+        },
+    }
+}
+
+export default function nutritionGraph({ props }) {
+
+    const nutritionData = Object.entries(props);
+    const router = useRouter();
+    const [mealName, setMealName] = useState([]);
+    const [pieData, setPieData] = useState([]);
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    let date = Date.parse(selectedDate);
+    let strDate = new Date(date).toLocaleDateString();
+
+    function dateChange(){
+        date = Date.parse(selectedDate);
+        strDate = new Date(date).toLocaleDateString();
+        alert(strDate);
     }
 
-    usersMeal = ['Grain', 'Beef', 'Lamb', 'vegetable'];//should be connected with the backend
-    mealsCal = [100, 1000, 500, 200];//should be connected with the backend
-    username = "Tom";//should be connected with the backend
+    useEffect(() => {
+        if (router.isReady) {
+            
+            dateChange();
 
-    componentDidMount() {
-        var usedata =[];
-        for(var i=0; i<this.usersMeal.length; i++){
-            usedata.push({name: this.usersMeal[i],value:this.mealsCal[i]})
-        }
-        this.setState(
-            {
-                piedata: usedata,
-                meal: this.usersMeal,
-                ID: this.username
+            const mealname_buf = [];
+            const piedata_buf = [];
+
+            for (let i = 0; i < nutritionData.length; i++) {
+                console.log(moment(nutritionData[i][1].date).format("YYYY/MM/DD"));
+                console.log(moment(selectedDate).format("YYYY/MM/DD"));
+                if (nutritionData[i][1].username == router.query.username && moment(nutritionData[i][1].date).format("YYYY/MM/DD") == moment(selectedDate).format("YYYY/MM/DD")) {
+                    const one_usermeals = nutritionData[i][1].meals;
+                    for(let j=0; j < one_usermeals.length; j++){
+                        
+                        const permealData = Object.entries(one_usermeals[j]);
+
+
+                        const oneMealData={}
+                        oneMealData.name = permealData[0][1];
+                        oneMealData.value = parseInt(permealData[1][1]);
+                        
+                        piedata_buf.push(oneMealData);
+                        mealname_buf.push(permealData[0][1]);
+                    }
+                    setMealName(mealname_buf);
+                    setPieData(piedata_buf);
+                }
             }
-        )
-    };
+        }
+    }, [router.isReady,selectedDate]);
 
-    getOption = (piedata,meal)=>{
-        let option = {
+    let option = {
             tooltip : {
                 trigger: 'item',
                 formatter: "{a} <br/>{b} : {c} ({d}%)" 
@@ -45,39 +79,34 @@ class workOutEcharts extends Component {
                 orient: 'vertical',
                 top: 20,
                 right: 5,
-                data: meal
+                data: mealName
             },
             series : [
                 {
                     name:'Nutrition Data',
                     type:'pie',
-                    data:piedata,
+                    data:pieData
                 }
             ]
-        }
-        return option;
     }
 
-    render() {
-        const{ID}=this.state;
-        const{piedata}=this.state;
-        const{meal}=this.state;
-        return (
+    return (
         <div>
             <Navbar bg="dark" variant="dark">
-                    <Container fluid>
-                        <Navbar.Brand href="#">
-                            <FontAwesomeIcon icon={faDumbbell} /> Workout Tracker
-                        </Navbar.Brand>
-                    </Container>
+                <Container fluid>
+                    <Navbar.Brand href="#">
+                        <FontAwesomeIcon icon={faDumbbell} /> Workout Tracker
+                    </Navbar.Brand>
+                </Container>
             </Navbar>
-            <div className="graph">
-                <h1 align={"center"}>{ID}'s Nutrition graph</h1>
-                <ReactEcharts option={this.getOption(piedata,meal)} />
-            </div>
-        </div>
-        );
-    }
-}
+            <h1></h1>
 
-export default workOutEcharts;
+            <DatePicker
+                selected={selectedDate}
+                onChange={date => setSelectedDate(date)}
+                dateFormat="MMMM d, yyyy"
+            />
+            <ReactEcharts option={option} />
+        </div>
+    )
+}
