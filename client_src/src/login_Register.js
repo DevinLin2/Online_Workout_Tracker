0eff77972f9d28b03d1abb5b81393171aeda1b9a
Devin Lin <43054186+DevinLin2@users.noreply.github.com>
2022-12-08 18:17:50 -0500

    Update login_Register.js

--- client_src/src/login_Register.js
+++ client_src/src/login_Register.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 
 const mysqlConfig = {
     host: "127.0.0.1",
-    port: 3306, // could remove this if bugs
+    port: 3306,
     user: "root",
     password: "",
     database: "workout",
